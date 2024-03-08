67b191c08a5015a706a74345be912c49f393c647
anhtuan512002 <20522114@gm.uit.edu.vn>
2024-03-09 02:59:58 +0700

    update_1

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express')
+const morgan=require('morgan')
 const app = express()
 const port = 3000
 //get / is the path of the url
 //res is a function that send back the respones to the cilents
 //
+
+app.use(morgan('combined'))
+
 app.get('/tin-tuc', (req, res) => {
-  var a=1;
-  var b=2;
-  var c=a+b;
-  return res.send('Tôi tên là tuấn');
+
+  return res.send(`
+  <h1 style="color:red">hello</h1>
+  `);
 })
 
 app.listen(port, () => {
