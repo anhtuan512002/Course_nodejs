b51ebe812c3a26368b36dcbcaa29fbb921e753d7
anhtuan512002 <20522114@gm.uit.edu.vn>
2024-03-09 22:21:26 +0700

    a

--- src/index.js
+++ src/index.js
@@ -4,23 +4,33 @@ const morgan=require('morgan')
 const handlebars  = require('express-handlebars');
 const app = express()
 const port = 3000
-//get / is the path of the url
-//res is a function that send back the respones to the cilents
-//
-//http logger
+//express sẽ kiểm tra đường dẫn tới thư mục public
+
+app.use(express.static(path.join(__dirname, '/public/')))
+
+console.log(__dirname)
 app.use(morgan('combined'))
 
 // template engine
-app.engine('handlebars', handlebars.engine());
+app.engine('hbs', handlebars.engine(
+{
+extname: '.hbs',
+}
+));
+
 // đặt cho view engine là handlebars
-app.set('view engine', 'handlebars');
+app.set('view engine', 'hbs');
 
-
-app.set('views', path.join(__dirname, './resources/views/'));
+app.set('views', path.join(__dirname, './resource/views/'));
 // xuất ra các file nằm trong thư mục view
 
 app.get('/', (req, res) => {
-  res.render('home.handlebars')
+  res.render('home')
+})
+
+
+app.get('/news', (req, res) => {
+  res.render('news')
 })
 
 app.listen(port, () => {
